f918dec7c3b4219ff2736a34f89084cb9e18967f
Runninginsilence1 <runninginsilence0@gmail.com>
2025-04-29 13:36:33 +0800

    sync

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,17 +1,13 @@
 ﻿<script lang="ts" setup>
+
 import myrequest from "@/utils/request";
 import {LewAlert, LewButton, LewMessage} from "lew-ui";
+import { ref } from "vue";
+import FindServerView from "@/views/FindServerView.vue"
 
 const tipsConsoleFunc = () => {
   LewMessage.success({
     content: '请在控制台中查看',
-    duration: 3000
-  })
-}
-
-const resultFunc = (data: any) => {
-  LewMessage.success({
-    content: data.toString(),
     duration: 3000
   })
 }
@@ -42,11 +38,27 @@ const testFunc = () => {
 
       });
 }
+
+// 
+const open = ref<boolean>(false);
+
+const showModal = () => {
+  open.value = true;
+};
+
+const handleOk = (e: MouseEvent) => {
+  console.log(e);
+  open.value = false;
+};
+
 </script>
 
 <template>
   <lew-button size="medium" :request="testFunc" text="调试按钮" type="ghost"/>
-<!--  <lew-button size="medium" :request="tipsConsoleFunc" text="调试按钮" type="ghost"/>-->
+<a-button type="primary" @click="showModal">Open Modal</a-button>
+<a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
+  <FindServerView />
+    </a-modal>
 </template>
 
 <style scoped lang="scss">
